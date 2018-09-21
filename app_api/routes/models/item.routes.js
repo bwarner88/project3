8aca048916275a017393e56dfe92b9ad62a48ee3
unknown <brian_warner25@yahoo.com>
2018-09-20 20:20:00 -0700

    item/user query

--- app_api/routes/models/item.routes.js
+++ app_api/routes/models/item.routes.js
@@ -9,6 +9,8 @@ router.post("/", ctrl.createItem);
 router.get("/:id", ctrl.findOneItem);
 /**GET ALL Item data from Item table */
 router.get("/", ctrl.findAllItem);
+/**GET ALL Item data from Item table by User */
+router.get("/user/item/:id", ctrl.findItemByUser);
 /**UPDATE Item WHERE Item is email FROM Item table */
 router.put("/:id", ctrl.updateItem);
 /**DELETE Item WHERE email is Item FROM Item table */
